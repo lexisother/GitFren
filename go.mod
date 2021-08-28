53cf48846e23365f91e356213958062d27c4babd
Alyxia Sother <lexisoth2005@gmail.com>
2021-08-28 13:29:51 +0200

    [Main] Create window

---
+++ go.mod
@@ -0,0 +1,13 @@
+module git.nova-vps.ml/lexisother/GitFren
+
+go 1.17
+
+require (
+	github.com/20kdc/CCUpdaterUI v0.0.0-20210313044331-b5003a3949b9 // indirect
+	github.com/20kdc/go-vkv v0.0.0-20191224143813-9318077c06cf // indirect
+	github.com/CCDirectLink/CCUpdaterCLI v1.2.1-0.20191206165254-1897428c31cd // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/veandco/go-sdl2 v0.4.10 // indirect
+	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d // indirect
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+  "github.com/20kdc/CCUpdaterUI/design"
+  "github.com/20kdc/CCUpdaterUI/frenyard"
+  "github.com/20kdc/CCUpdaterUI/frenyard/framework"
+  "git.nova-vps.ml/lexisother/GitFren/middle"
+)
+
+// Code in this file was taken from CCUpdaterUI/main.go
+
+type upApplication struct {
+  config middle.UpdaterConfig
+  mainContainer *framework.UISlideTransitionContainer
+  window frenyard.Window
+  upQueued chan func()
+  teleportSettings framework.SlideTransition
+}
+
+const upTeleportLen float64 = 0.25
+
+// GSLeftwards sets the teleportation affinity to LEFT.
+func (app *upApplication) GSLeftwards() {
+	app.teleportSettings.Reverse = true
+	app.teleportSettings.Vertical = false
+	app.teleportSettings.Length = upTeleportLen
+}
+// GSRightwards sets the teleportation affinity to RIGHT.
+func (app *upApplication) GSRightwards() {
+	app.teleportSettings.Reverse = false
+	app.teleportSettings.Vertical = false
+	app.teleportSettings.Length = upTeleportLen
+}
+// GSLeftwards sets the teleportation affinity to UP.
+func (app *upApplication) GSUpwards() {
+	app.teleportSettings.Reverse = true
+	app.teleportSettings.Vertical = true
+	app.teleportSettings.Length = upTeleportLen
+}
+// GSRightwards sets the teleportation affinity to DOWN.
+func (app *upApplication) GSDownwards() {
+	app.teleportSettings.Reverse = false
+	app.teleportSettings.Vertical = true
+	app.teleportSettings.Length = upTeleportLen
+}
+// GSInstant sets the teleportation affinity to INSTANT.
+func (app *upApplication) GSInstant() {
+	// direction doesn't matter
+	app.teleportSettings.Length = 0
+}
+// Teleport starts a transition with the cached affinity settings.
+func (app *upApplication) Teleport(target framework.UILayoutElement) {
+	forkTD := app.teleportSettings
+	forkTD.Element = target
+	app.mainContainer.TransitionTo(forkTD)
+}
+
+func main() {
+	frenyard.TargetFrameTime = 0.016
+	slideContainer := framework.NewUISlideTransitionContainerPtr(nil)
+	slideContainer.FyEResize(design.SizeWindowInit)
+	wnd, err := framework.CreateBoundWindow("CCUpdaterUI", true, design.ThemeBackground, slideContainer)
+	if err != nil {
+		panic(err)
+	}
+	design.Setup(frenyard.InferScale(wnd))
+	wnd.SetSize(design.SizeWindow)
+	// Ok, now get it ready.
+	app := (&upApplication{
+		config: middle.ReadUpdaterConfig(),
+		mainContainer: slideContainer,
+		window: wnd,
+		upQueued: make(chan func(), 16),
+		teleportSettings: framework.SlideTransition{
+		},
+	})
+	// Started!
+	frenyard.GlobalBackend.Run(func(frameTime float64) {
+		select {
+			case fn := <- app.upQueued:
+				fn()
+			default:
+		}
+	})
+}
