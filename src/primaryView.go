0ba0a85a7c7c4198b32bf6b8fd01135d8c34bfa6
Alyxia Sother <lexisoth2005@gmail.com>
2021-08-29 20:54:39 +0200

    [primaryView] Display list of public repos

--- src/primaryView.go
+++ src/primaryView.go
@@ -1,19 +1,22 @@
 package src
 
 import (
-	"github.com/20kdc/CCUpdaterUI/frenyard/framework"
 	"github.com/20kdc/CCUpdaterUI/design"
+	"github.com/20kdc/CCUpdaterUI/frenyard/framework"
+	"git.nova-vps.ml/lexisother/GitFren/middle"
 )
 
 func (app *UpApplication) ShowPrimaryView() {
 	slots := []framework.FlexboxSlot{}
 
-	slots = append(slots, framework.FlexboxSlot{
-		Element: design.ListItem(design.ListItemDetails{
-			Text: "Yes",
-			Subtext: "Uh huh",
-		}),
-	})
+	repoNames := middle.GetRepoNames()
+	for _, name := range repoNames {
+		slots = append(slots, framework.FlexboxSlot{
+			Element: design.ListItem(design.ListItemDetails{
+				Text: name,
+			}),
+		})
+	}
 
 	app.Teleport(design.LayoutDocument(design.Header{
 		Title: "GitFren",
