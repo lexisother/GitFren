262082ae9f1b69cca743899b81e946cd8f638e9a
Alyxia Sother <lexisoth2005@gmail.com>
2021-08-30 10:17:49 +0200

    [middle] Use /explore/users page instead of repos

--- middle/repos.go
+++ middle/repos.go
@@ -7,19 +7,20 @@ import (
 	"net/http"
 	"regexp"
 
+	"git.nova-vps.ml/lexisother/GitFren/middle/api"
 	"github.com/anaskhan96/soup"
 )
 
-func GetRepoNames() []string {
-	resp, _ := soup.Get("https://git.nova-vps.ml/explore/repos")
+func GetUserNames() []string {
+	resp, _ := soup.Get("https://git.nova-vps.ml/explore/users")
 	final := []string{}
 
 	doc := soup.HTMLParse(resp)
-	links := doc.Find("div", "class", "repository").FindAll("div", "class", "item")
+	links := doc.Find("div", "class", "user").FindAll("div", "class", "item")
 	for _, link := range links {
-		names := link.FindAll("a", "class", "name")
+		names := link.FindAll("span", "class", "header")
 		for _, name := range names {
-			text := name.Text()
+			text := name.Find("a").Text()
 			m1 := regexp.MustCompile(`\s`)
 			text = m1.ReplaceAllString(text, "")
 			final = append(final, text)
@@ -29,8 +30,10 @@ func GetRepoNames() []string {
 	return final
 }
 
-func GetRepoData(url string, repoName string) Repository {
-	resp, err := http.Get(url + "/repos/" + repoName)
+func GetUserRepos(url string, user string) []api.Repository {
+	var repos []api.Repository
+
+	resp, err := http.Get(fmt.Sprintf("%s/users/%s/repos", url, user))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -39,7 +42,22 @@ func GetRepoData(url string, repoName string) Repository {
 
 	body, err := io.ReadAll(resp.Body)
 
-	var repo Repository
+	json.Unmarshal([]byte(body), &repos)
+
+	return repos
+}
+
+func GetRepoData(url string, owner string, repoName string) api.Repository {
+	resp, err := http.Get(fmt.Sprintf("%s/%s/%s", url, owner, repoName))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	var repo api.Repository
 	json.Unmarshal([]byte(body), &repo)
 
 	return repo
